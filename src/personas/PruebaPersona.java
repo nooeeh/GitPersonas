4b0475a195f8666bded08041d18fd56e12e73848
nooeeh <noejorq@gmail.com>
2025-12-07 22:22:29 +0100

    Update PruebaPersona.java

--- src/personas/PruebaPersona.java
+++ src/personas/PruebaPersona.java
@@ -17,7 +17,7 @@ public class PruebaPersona {
          // Creamos varias instancias de personas
          Persona per1 = new Persona("20222444L", "María", "Carcelén Sánchez", 67);
          Persona r2 = new Persona("José", "Serrano Márquez", 25);
-         Persona per3 = new Persona("64578488K","Pedro", "De la Calle García", 55);
+         Persona hola = new Persona("64578488K","Pedro", "De la Calle García", 55);
          
          //Almacenamos las instancias de personas en un array
          Persona[] arrayPersonas = new Persona[3];
